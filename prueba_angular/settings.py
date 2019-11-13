ef0bfa175fd1b7de56d77b016fc1296a215edafa
Jonathan Cortés <jonathan.cortes@inmersa.co>
2019-11-12 22:24:08 -0500

    CSRF disable

--- prueba_angular/settings.py
+++ prueba_angular/settings.py
@@ -110,8 +110,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
